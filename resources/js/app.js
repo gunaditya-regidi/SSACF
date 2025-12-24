ea7b03553c31a68d9d70abf1317e3be3ab8d7d0e
aditya.regidi.001 <aditya.regidi.001@gmail.com>
2025-12-24 19:08:12 +0000

    n

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,10 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel';
 
+import.meta.glob([
+    '../images/**',
+  ]);
+
 $(document).ready(function(){
     // Splash Screen
     setTimeout(function () {
