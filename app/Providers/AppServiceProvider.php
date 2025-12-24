030f72d33b499e40b112983b251afca8d13a742e
aditya.regidi.001 <aditya.regidi.001@gmail.com>
2025-12-24 18:51:50 +0000

    Fix: Use relative asset URLs in production to resolve image loading issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            $this->app['config']->set('app.asset_url', '/');
+        }
     }
 
     /**
