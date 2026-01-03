9c9dba6e7a85b0257c4d47f935b8f5053d3dd26c
aditya.regidi.001 <aditya.regidi.001@gmail.com>
2026-01-03 14:16:40 +0000

    few final changes done

--- resources/views/components/dignity-comfort-compassion.blade.php
+++ resources/views/components/dignity-comfort-compassion.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="dignity-content">
             <h2 class="dignity-headline">Providing Care with Compassion and Honouring Dignity of Life</h2>
-            <p class="dignity-description">At Sneha Sandhya Age Care Foundation, we believe that every life deserves to be lived with care, dignity and comfort— even in the face of serious illness. Through holistic supportive specialised medical care, we serve the elderly and the seriously ill with compassion, offering relief from suffering and strength to families during life’s most vulnerable journeys.”</p>
+            <p class="dignity-description text-justify">At Sneha Sandhya Age Care Foundation, we believe that every life deserves to be lived with care, dignity and comfort— even in the face of serious illness. Through holistic supportive specialised medical care, we serve the elderly and the seriously ill with compassion, offering relief from suffering and strength to families during life’s most vulnerable journeys.”</p>
             <div class="dignity-tags">
                 <span class="dignity-tag">Palliative Care</span>
                 <span class="dignity-tag">Geriatric Care</span>
